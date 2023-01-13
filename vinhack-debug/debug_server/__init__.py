7a62efdc6780391a68d1c1c942780929578bdfb8
Naman Agrawal <a6.namanagrawal@gmail.com>
2023-01-13 22:09:26 +0530

    Feat: variables

--- vinhack-debug/debug_server/__init__.py
+++ vinhack-debug/debug_server/__init__.py
@@ -21,6 +21,8 @@ def pdb_init():
     while not re.search("\(Pdb\)", info):
         info += pdb_shell.stdout.read(1).decode()
     line_number = int(re.search("\((\d+)\)", info).group()[1:-1])
+    pdb_shell.stdin.write(b"import json\n")
+    pdb_shell.stdin.flush()
     message = "Initialized" 
     return message
 
@@ -42,13 +44,32 @@ def next_line():
     while not re.search("\(\d+\)<module>\(\)", info):
         char = pdb_shell.stdout.read(1).decode()
         if char == '\n':
-            if re.search("\(Pdb\)", info):
-                output += info[5::]
+            if re.search("\(Pdb\) ", info):
+                output += info[6::]
             info = ""
         else: info += char
     line_number = int(re.search("\((\d+)\)", info).group()[1:-1])
     response = {"output": output, "linenumber": line_number}
     return json.dumps(response) 
+
+@app.route("/print/<variable_name>")
+def print_variable(variable_name):
+    pdb_shell.stdin.write(bytes("p json.dumps(" + variable_name + ")\n", 'utf-8'))
+    pdb_shell.stdin.flush()
+    info = ""
+    while not re.search("\(Pdb\) ", info):
+        info += pdb_shell.stdout.read(1).decode()
+    return pdb_shell.stdout.readline().decode()
+
+@app.route("/variables")
+def all_variables():
+    pdb_shell.stdin.write(b"print(json.dumps([x for x in locals().keys()]))\n")
+    pdb_shell.stdin.flush()
+    info = ""
+    while not re.search("\(Pdb\) ", info):
+        info += pdb_shell.stdout.read(1).decode()
+    return pdb_shell.stdout.readline().decode()
+
 
 @app.route("/source", methods=["GET"])
 def source():
